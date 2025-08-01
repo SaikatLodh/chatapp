b8beeb587ad556e1a184f3ff46f1c408f0e0942b
saikatlodh8@gmail.com <saikatlodh8@gmail.com>
2025-08-01 20:25:58 +0530

    cors change

--- server/app/app.js
+++ server/app/app.js
@@ -22,6 +22,19 @@ const io = new Server(server, {
   },
 });
 setIO(io);
+const allowedOrigins = [process.env.CLIENT_URL, "*"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "*",
