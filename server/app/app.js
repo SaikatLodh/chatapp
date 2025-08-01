53bcc3de77874190b49f55b23ec1d9d065028509
saikatlodh8@gmail.com <saikatlodh8@gmail.com>
2025-08-01 20:30:41 +0530

    change origin

--- server/app/app.js
+++ server/app/app.js
@@ -16,28 +16,16 @@ env.config({ path: ".env" });
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "*",
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   },
 });
 setIO(io);
-const allowedOrigins = [process.env.CLIENT_URL, "*"];
+
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
